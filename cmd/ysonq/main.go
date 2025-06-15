2b62093bfe8ae68aa924389ffd9daeafc2d24e6c
Denis Sheremet <gitlab@lxlz.space>
2025-06-16 00:24:34 +0300

    less of intermediate representations

--- cmd/ysonq/main.go
+++ cmd/ysonq/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -17,42 +16,17 @@ func main() {
 		panic(err)
 	}
 
-	// gojq won't work directly with yson types
-	jsonInput, err := common.Chain(common.YSON, common.JSON, common.FormatText)(input)
+	parsed, err := common.ParseYson(input)
 	if err != nil {
 		panic(err)
 	}
 
-	var parsed any
-	err = json.Unmarshal(jsonInput, &parsed)
+	query, err := gojq.Parse(".")
 	if err != nil {
 		panic(err)
 	}
 
-	query, err := gojq.Parse(".[2].Attrs")
-	if err != nil {
-		panic(err)
-	}
-
-	normalizeQuery, err := gojq.Parse(`
-	walk(
-		if type == "object"
-		then with_entries(
-			if .key == "Attrs" then .key = "$attributes"
-			elif .key == "Value" then .key = "$value"
-			else . end
-		) else . end
-	)`)
-	if err != nil {
-		panic(err)
-	}
-
-	normalizer, err := gojq.Compile(normalizeQuery)
-	if err != nil {
-		panic(err)
-	}
-
-	iter := query.Run(parsed)
+	iter := query.Run(NormalizeYSON(parsed))
 	for {
 		v, ok := iter.Next()
 		if !ok {
@@ -64,29 +38,10 @@ func main() {
 			}
 			log.Fatalln(err)
 		}
-
-		normalizedIter := normalizer.Run(v)
-		for {
-			v, ok := normalizedIter.Next()
-			if !ok {
-				break
-			}
-			if err, ok := v.(error); ok {
-				if err, ok := err.(*gojq.HaltError); ok && err.Value() == nil {
-					break
-				}
-				log.Fatalln(err)
-			}
-
-			json, err := common.FormatText.JsonMarhaler()(v)
-			if err != nil {
-				panic(err)
-			}
-			yson, err := common.Chain(common.JSON, common.YSON, common.FormatPretty)(json)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println(string(yson))
+		result, err := common.FormatPretty.YsonMarshaler()(DenormalizeYSON(v))
+		if err != nil {
+			panic(err)
 		}
+		fmt.Println(string(result))
 	}
 }
---
+++ cmd/ysonq/normalizer.go
@@ -0,0 +1,58 @@
+package main
+
+import "go.ytsaurus.tech/yt/go/yson"
+
+const (
+	valueKey = "Value"
+	attrsKey = "Attrs"
+)
+
+func NormalizeYSON(v any) any {
+	switch v := v.(type) {
+	case *yson.ValueWithAttrs:
+		return map[string]any{
+			valueKey: v.Value,
+			attrsKey: v.Attrs,
+		}
+	case []any:
+		for i, x := range v {
+			v[i] = NormalizeYSON(x)
+		}
+		return v
+	case map[string]any:
+		for k, x := range v {
+			v[k] = NormalizeYSON(x)
+		}
+		return v
+	default:
+		return v
+	}
+}
+
+func DenormalizeYSON(v any) any {
+	switch v := v.(type) {
+	case []any:
+		for i, x := range v {
+			v[i] = DenormalizeYSON(x)
+		}
+		return v
+	case map[string]any:
+		// check if map is actually a value with attributes
+		value, hasValue := v[valueKey]
+		attrs, hasAttrs := v[attrsKey]
+
+		if hasValue && hasAttrs {
+			return &yson.ValueWithAttrs{
+				Value: value,
+				Attrs: attrs.(map[string]any),
+			}
+		}
+
+		for k, x := range v {
+			v[k] = DenormalizeYSON(x)
+		}
+		return v
+	default:
+		return v
+	}
+}
